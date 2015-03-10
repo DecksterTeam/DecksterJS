67791450464f2dd0e0462f2d7ef3437d39efc4cf
floyd_hawkes <fhawkes@42six.com>
2015-03-10 10:40:19 -0400

    feat(card): added base for prepackaged map deckster map card

--- dist/cards/map/map.js
+++ dist/cards/map/map.js
@@ -1,3 +1,36 @@
+;(function (root, factory) {
+
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery', 'deckster'], factory);
+  } else {
+    root.DecksterMapCard = factory(root.$ || root.jQuery, root.Deckster);
+  }
+
+}(this, function($, Deckster){
+
+  // Card config for map
+  Deckster.cards.map = {
+    title: 'Map',
+    position: {
+      size_x: 3,
+      size_y: 2
+    },
+    summaryContentHtml: function (cardConfig, cb) {
+
+    },
+    detailsContentHtml: function (cardConfig, cb) {
+
+    },
+    onSummaryLoad: function (card) {
+
+    },
+    onDetailsLoad: function (card) {
+
+    }
+  };
+
+}));
+
 (function() {
 window["Deckster"] = window["Deckster"] || {};
 window["Deckster"]["Templates"] = window["Deckster"]["Templates"] || {};
